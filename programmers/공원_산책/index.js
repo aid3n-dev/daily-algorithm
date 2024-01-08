1f807690c05d2b5bbdb5baf80985e8c7623e9207
aid3n-dev <aid3n.dev@gmail.com>
2024-01-08 16:43:31 +0900

    refactoring. 공원 산책

--- programmers/공원_산책/index.js
+++ programmers/공원_산책/index.js
@@ -1,19 +1,43 @@
-function solution(park, routes) {
-  const H = park.length;
-  const W = park[0].length;
-  let [positionX, positionY] = park.reduce(
+const findStartPosition = (park) => {
+  return park.reduce(
     (position, row, rowIndex) => {
       const colIndex = row.indexOf("S");
       return colIndex !== -1 ? [rowIndex, colIndex] : position;
     },
     [0, 0]
   );
+};
+
+function solution(park, routes) {
+  const H = park.length;
+  const W = park[0].length;
+  let [positionX, positionY] = findStartPosition(park);
+
+  const canMove = (dx, dy, steps) => {
+    let newX = positionX;
+    let newY = positionY;
+
+    for (let i = 0; i < steps; i++) {
+      newX += dx;
+      newY += dy;
+
+      if (
+        newY >= W ||
+        newY < 0 ||
+        newX >= H ||
+        newX < 0 ||
+        park[newX][newY] === "X"
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  };
 
   routes.forEach((route) => {
     const [direction, steps] = route.split(" ");
     const numberSteps = Number(steps);
-    const startPositionX = positionX;
-    const startPositionY = positionY;
 
     let dx = 0;
     let dy = 0;
@@ -22,28 +46,10 @@ function solution(park, routes) {
     if (direction === "S") dx = 1;
     if (direction === "W") dy = -1;
     if (direction === "E") dy = 1;
-    let isEscape = false;
 
-    for (let i = 0; i < numberSteps; i++) {
-      if (
-        positionY + dy >= W ||
-        positionY + dy < 0 ||
-        positionX + dx >= H ||
-        positionX + dx < 0 ||
-        park[positionX + dx][positionY + dy] === "X"
-      ) {
-        isEscape = true;
-
-        break;
-      }
-
-      positionX += dx;
-      positionY += dy;
-    }
-
-    if (isEscape) {
-      positionX = startPositionX;
-      positionY = startPositionY;
+    if (canMove(dx, dy, numberSteps)) {
+      positionX += dx * steps;
+      positionY += dy * steps;
     }
   });
 
