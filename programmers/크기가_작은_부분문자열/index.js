fe0cfb77ffe5c0ccd5b281879c59d4307be40613
aid3n-dev <aid3n.dev@gmail.com>
2024-01-10 16:45:56 +0900

    solve. 크기가 작은 부분문자열

---
+++ programmers/크기가_작은_부분문자열/index.js
@@ -0,0 +1,18 @@
+function solution(t, p) {
+  const strLength = p.length;
+  let result = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    const slicedStr = t.substr(i, strLength);
+
+    if (slicedStr.length === strLength) {
+      if (Number(slicedStr) <= Number(p)) {
+        result++;
+      }
+    }
+  }
+
+  console.log(result);
+}
+
+solution("3141592", "271");
